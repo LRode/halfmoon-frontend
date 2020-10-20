9c1eca4968b28f784799147a553fb03cfefb6080
Lara Rode <lara.rode@outlook.com>
2020-10-20 10:24:15 -0700

    Responsive navbar functionality

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,86 @@
 import Link from 'next/link'
-import styles from '../styles/Header.module.css'
+import { useState, useRef } from 'react';
+
+import styles from '../styles/Header.module.css';
+import useOnClickOutside from '../hooks/useOnClickOutside.js';
 
 export default function Header({ activePage }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const mobileNavMenuRef = useRef(null);
+    useOnClickOutside(mobileNavMenuRef, () => setIsOpen(false));
+
     return (
-        <div>
+        <React.Fragment>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             <script src="https://kit.fontawesome.com/a076d05399.js" />
-                <navBar className={styles.navBar}>
-                    <div className={styles.menuIcon} id="menuBtn">
-                        <span class="fas fa-bars"></span></div>
+            <nav className={styles.navBar}>
+                <button
+                    className={styles.menuIcon}
+                    onClick={() => setIsOpen(!isOpen)}>
+                    <span class="fas fa-bars"></span>
+                </button>
 
-                    <img src="/HMLogo.svg" alt="logo" className={styles.logo} />
+                <img src="/HMLogo.svg" alt="logo" className={styles.logo} />
 
-                    <div className={styles.navLinks}>
-                        <Link href="/">
-                            <a className={`${styles.links} ${activePage === 'Home' ? styles.activeLink : ''}`}>
-                                Home
+                <div className={styles.navLinks}>
+                    <Link href="/">
+                        <a className={`${styles.links} ${activePage === 'Home' ? styles.activeLink : ''}`}>
+                            Home
                         </a>
-                        </Link>
-                        <Link href="/products">
-                            <a className={`${styles.links} ${activePage === 'Products' ? styles.activeLink : ''}`}>
-                                Products
+                    </Link>
+                    <Link href="/products">
+                        <a className={`${styles.links} ${activePage === 'Products' ? styles.activeLink : ''}`}>
+                            Products
                         </a>
-                        </Link>
-                        <Link href="/blog">
-                            <a className={`${styles.links} ${activePage === 'Blog' ? styles.activeLink : ''}`}>
-                                Blogs
+                    </Link>
+                    <Link href="/blog">
+                        <a className={`${styles.links} ${activePage === 'Blog' ? styles.activeLink : ''}`}>
+                            Blogs
                         </a>
-                        </Link>
-                        <Link href="/blog">
-                            <a className={`${styles.links} ${activePage === 'Contact Us' ? styles.activeLink : ''}`}>
-                                Contact Us
+                    </Link>
+                    <Link href="/blog">
+                        <a className={`${styles.links} ${activePage === 'Contact Us' ? styles.activeLink : ''}`}>
+                            Contact Us
                         </a>
-                        </Link>
-                    </div>
-                    <div className={styles.searchIcon}>
-                        <span class="fas fa-search"></span></div>
-                    <div className={styles.cancelIcon}>
-                        <span class="fas fa-times"></span></div>
+                    </Link>
+                </div>
 
-                    <form action="/search" method="get" className={styles.searchForm}>
-                        <input type="search" name="search" placeholder="Search..." className={styles.searchInput} />
-                        <button type="submit" className={styles.formButton}><i class="fas fa-search"></i></button>
-                    </form>
+                <div className={styles.searchIcon}>
+                    <span class="fas fa-search"></span></div>
+                <div className={styles.cancelIcon}>
+                    <span class="fas fa-times"></span></div>
 
-                </navBar>  
-        </div>
-        
+                <form action="/search" method="get" className={styles.searchForm}>
+                    <input type="search" name="search" placeholder="Search..." className={styles.searchInput} />
+                    <button type="submit" className={styles.formButton}><i class="fas fa-search"></i></button>
+                </form>
+
+                <div ref={mobileNavMenuRef} className={`${styles.mobileNavMenu} ${isOpen ? styles.open : ''}`}>
+                    <Link href="/">
+                        <a className={`${styles.links} ${activePage === 'Home' ? styles.activeLink : ''}`}>
+                            Home
+                        </a>
+                    </Link>
+                    <Link href="/products">
+                        <a className={`${styles.links} ${activePage === 'Products' ? styles.activeLink : ''}`}>
+                            Products
+                        </a>
+                    </Link>
+                    <Link href="/blog">
+                        <a className={`${styles.links} ${activePage === 'Blog' ? styles.activeLink : ''}`}>
+                            Blogs
+                        </a>
+                    </Link>
+                    <Link href="/blog">
+                        <a className={`${styles.links} ${activePage === 'Contact Us' ? styles.activeLink : ''}`}>
+                            Contact Us
+                        </a>
+                    </Link>
+                </div>
+            </nav>
+        </React.Fragment>
+
     )
 }
 
---
+++ hooks/useOnClickOutside.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+
+/**
+ * This hook allows you to detect clicks outside of a specified element
+ *
+ * @param {Node} ref  - Reference to element that we want to watch for when we click outside of
+ * @param {Function} handler - Function that handles what happens when a click outside happens
+ */
+function useOnClickOutside(ref, handler) {
+    useEffect(
+        () => {
+            const listener = event => {
+                // Do nothing if clicking ref's element or descendent elements
+                if (!ref.current || ref.current.contains(event.target)) {
+                    return;
+                }
+
+                handler(event);
+            };
+
+            document.addEventListener('mousedown', listener);
+            document.addEventListener('keydown', listener);
+            document.addEventListener('touchstart', listener);
+
+            return () => {
+                document.removeEventListener('mousedown', listener);
+                document.removeEventListener('keydown', listener);
+                document.removeEventListener('touchstart', listener);
+            };
+        },
+        [ref, handler]
+    );
+}
+
+export default useOnClickOutside;
