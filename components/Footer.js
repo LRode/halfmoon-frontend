772025e1bb7aa22ca9207dd4b2faf728f08751e4
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2020-11-03 17:42:47 +0100

    format footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,93 +3,93 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-            <footer className={styles.footer}>
+        <footer className={styles.footer}>
 
-                <div className={styles.topLine}>
-                    <div className={styles.logoBox}>
-                        <Link href="/"><img src="/HMLogo.svg" alt="logo" className={styles.logo} /></Link>
+            <div className={styles.topLine}>
+                <div className={styles.logoBox}>
+                    <Link href="/"><img src="/HMLogo.svg" alt="logo" className={styles.logo} /></Link>
 
-                    </div>
+                </div>
 
-                    <div className={styles.leftBox}>
+                <div className={styles.leftBox}>
 
-                        <img src="/mapicon.svg" className={styles.mapIcon} />
+                    <img src="/mapicon.svg" className={styles.mapIcon} />
 
-                        <div className={styles.addressBox}>
+                    <div className={styles.addressBox}>
 
-                            <p>8171 Main St</p>
-                            <p>Vancouver, BC</p>
-                            <p>VSX 3L2</p>
-
-                        </div>
-                        <div className={styles.hoursBox}>
-
-                            <p>Monday - Saturday</p>
-                            <p>Sunday</p>
-
-                        </div>
-                        <div className={styles.hoursBox}>
-
-                            <p>12 - 7 PM</p>
-                            <p>12 - 6 PM</p>
-
-                        </div>
+                        <p>8171 Main St</p>
+                        <p>Vancouver, BC</p>
+                        <p>VSX 3L2</p>
 
                     </div>
+                    <div className={styles.hoursBox}>
 
-                    <div className={styles.rightBox}>
+                        <p>Monday - Saturday</p>
+                        <p>Sunday</p>
 
-                        <div className={styles.navBox}>
+                    </div>
+                    <div className={styles.hoursBox}>
 
-                            <a href="/">Home</a>
-                            <a href="/products">Products</a>
-                            <a href="/blog">Blog</a>
-                            <a href="mailto:info@animanga.me">Contact Us</a>
-
-                        </div>
+                        <p>12 - 7 PM</p>
+                        <p>12 - 6 PM</p>
 
                     </div>
 
                 </div>
 
-                <hr className={styles.solid}/>
+                <div className={styles.rightBox}>
 
-                <div className={styles.bottomLine}>
+                    <div className={styles.navBox}>
 
-                    <div className={styles.bottomLeftBox}>
-                        <p>
-                            © Halfmoon, 2020
+                        <a href="/">Home</a>
+                        <a href="/products">Products</a>
+                        <a href="/blog">Blog</a>
+                        <a href="mailto:info@animanga.me">Contact Us</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <hr className={styles.solid} />
+
+            <div className={styles.bottomLine}>
+
+                <div className={styles.bottomLeftBox}>
+                    <p>
+                        © Halfmoon, 2020
                         </p>
 
+                </div>
+
+                <div className={styles.bottomRightBox}>
+
+                    <div>
+                        <img src="/phone.svg" alt="logo" className={styles.phone} />
                     </div>
 
-                    <div className={styles.bottomRightBox}>
+                    <div>
+                        <p>(604) 301-9075</p>
+                    </div>
 
-                        <div>
-                            <img src="/phone.svg" alt="logo" className={styles.phone} />
-                        </div>
+                    <div>
+                        <img src="/envelope.svg" alt="logo" className={styles.envelope} />
+                    </div>
 
-                        <div>
-                            <p>(604) 301-9075</p>
-                        </div>
+                    <div className={styles.email}>
+                        <Link href="mailto:info@animanga.me"><p>info@animanga.me</p></Link>
+                    </div>
 
-                        <div>
-                            <img src="/envelope.svg" alt="logo" className={styles.envelope} />
-                        </div>
-
-                        <div className={styles.email}>
-                            <Link href="mailto:info@animanga.me"><p>info@animanga.me</p></Link>
-                        </div>
-
-                        <div>
-                            <Link href="https://www.facebook.com/Halfmoon-Manga-Anime-21613267752/"><img src="/fbicon.svg" alt="logo" className={styles.fbIcon} /></Link>
-                        </div>
-
+                    <div>
+                        <Link href="https://www.facebook.com/Halfmoon-Manga-Anime-21613267752/"><img src="/fbicon.svg" alt="logo" className={styles.fbIcon} /></Link>
                     </div>
 
                 </div>
-                
+
+            </div>
+
         </footer>
-        
+
     )
 }
