604743d73e8808b3915f2cd9f8bb187e85b07dc5
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2020-12-05 01:41:56 +0100

    contact section Css and Map

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -2,96 +2,75 @@ import styles from '../styles/ContactSection.module.css';
 import PageTitle from '../components/pageTitle.js'
 import React from "react";
 import Link from 'next/link'
-import {withScriptjs,withGoogleMap,GoogleMap,Marker} from "react-google-maps";
-
-const defaultCenter = { lat: 40.748817, lng: -73.985428 };
-
-const defaultOptions = { scrollwheel: false };
-
-const RegularMap = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={8}
-      defaultCenter={ defaultCenter }
-      defaultOptions={ defaultOptions }
-    >
-      <Marker position={ defaultCenter } />
-    </GoogleMap>
-  ))
-);
-
-const loadingElementStyle = { height: '100%' };
-const containerElementStyle = { height: '100%' };
-const mapElementStyle = { height: '100%' };
 
 export default function ContactSection() {
 
     return (
         <div className={styles.container}>
-            <PageTitle title='Find Us' />
             <div className={styles.main}>
-                <div className={styles.contactBox}>
-                    <div className={styles.content}>
-                        <div className={styles.rowDisplay}>
-                            <div>
-                                <Link href="mailto:info@animanga.me"><img src="/envelope.svg" alt="logo" className={styles.envelope} /></Link>
-                            </div>
-                            <div className={styles.email}>
-                                <Link href="mailto:info@animanga.me">info@animanga.me</Link>
-                            </div>
-                        </div>
-                        <div className={styles.rowDisplay}>
-                            <div className={styles.addressContainer}>
+                <div className={styles.contactContainer}>
+                    <div>
+                        <PageTitle title='Find Us' />
+                    </div>
+                    <div className={styles.contactBox}>
+                        <div className={styles.content}>
+                            <div className={styles.rowDisplay}>
                                 <div>
-                                    <img src="/mapicon.svg" className={styles.mapIcon} />
+                                    <Link href="mailto:info@animanga.me"><img src="/envelope.svg" alt="logo" className={styles.envelope} /></Link>
                                 </div>
+                                <div className={styles.email}>
+                                    <Link href="mailto:info@animanga.me">info@animanga.me</Link>
+                                </div>
+                            </div>
+                            <div className={styles.rowDisplay}>
+                                <div className={styles.addressContainer}>
+                                    <div>
+                                        <img src="/mapicon.svg" className={styles.mapIcon} />
+                                    </div>
 
-                                <Link href="https://goo.gl/maps/eAUVuMgsLMhacw3S7">
-                                    <a target="_blank">
-                                        <div className={styles.addressBox}>
-                                            <p>8171 Main St</p>
-                                            <p>Vancouver, BC</p>
-                                            <p>VSX 3L2</p>
-                                        </div>
-                                    </a>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.content}>
-                        <div className={styles.rowDisplay}>
-                            <div>
-                                <Link href="tel:123-456-7890"><img src="/phone.svg" alt="logo" className={styles.phone} /></Link>
-                            </div>
-                            <div className={styles.phoneNum}>
-                                <Link href="tel:123-456-7890">(604) 301-9075</Link>
-                            </div>
-                        </div>
-                        <div className={styles.rowDisplay}>
-                            <div className={styles.hoursContainer}>
-                                <div className={styles.hoursBox}>
-                                    <h3>Hours</h3>
-                                    <p>Monday - Saturday</p>
-                                    <p>Sunday</p>
-                                </div>
-                                <div className={styles.hoursTime}>
-                                    <p>12 - 7 PM</p>
-                                    <p>12 - 6 PM</p>
+                                    <Link href="https://goo.gl/maps/eAUVuMgsLMhacw3S7">
+                                        <a target="_blank">
+                                            <div className={styles.addressBox}>
+                                                <p>8171 Main St</p>
+                                                <p>Vancouver, BC</p>
+                                                <p>VSX 3L2</p>
+                                            </div>
+                                        </a>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
+                        <div className={styles.content}>
+                            <div className={styles.rowDisplay}>
+                                <div>
+                                    <Link href="tel:123-456-7890"><img src="/phone.svg" alt="logo" className={styles.phone} /></Link>
+                                </div>
+                                <div className={styles.phoneNum}>
+                                    <Link href="tel:123-456-7890">(604) 301-9075</Link>
+                                </div>
+                            </div>
+                            <div className={styles.rowDisplay}>
+                                <div className={styles.hoursContainer}>
+                                    <div className={styles.hoursBox}>
+                                        <h3>Hours</h3>
+                                        <p>Monday - Saturday</p>
+                                        <p>Sunday</p>
+                                    </div>
+                                    <div className={styles.hoursTime}>
+                                        <p>12 - 7 PM</p>
+                                        <p>12 - 6 PM</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
+
                 <div className={styles.mapBox}>
-                    <RegularMap
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7E21PtntMkoiE9WN53w7AaP6g2QTNsUA"
-                        loadingElement={<div style={loadingElementStyle} />}
-                        containerElement={<div style={containerElementStyle} />}
-                        mapElement={<div style={mapElementStyle} />}
-                    />
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2606.4076630872037!2d-123.10477758413717!3d49.21179538371143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548674f6a6909e23%3A0x3d08bd25311f560!2sHalf%20Moon%20Bookstore!5e0!3m2!1sen!2spl!4v1607121652060!5m2!1sen!2spl" width="100%" height="100%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
-
         </div>
     )
 }
